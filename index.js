2b590d586b2dd28f2860012279751437e1f4088c
Gachure <your.email@example.com>
2023-12-19 12:38:26 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      // function implementation
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function () {
+      // function implementation
+    };
+  }
+  
+  // export the functions if needed
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction,
+  };
+  
