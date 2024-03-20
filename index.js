54c96c73953c7493db929585493c06b1355dfebd
victor <victor.gachure@student.moringaschool>
2024-03-20 08:09:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // index.js
-
 function receivesAFunction(callback) {
     callback();
   }
-  
   function returnsANamedFunction() {
     return function namedFunction() {
       // function implementation
     };
   }
-  
   function returnsAnAnonymousFunction() {
     return function () {
       // function implementation
     };
   }
-  
   // export the functions if needed
   module.exports = {
     receivesAFunction,
